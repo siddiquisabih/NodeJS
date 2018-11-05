11404f25be47c42958e8541cebe66d2d2da1cd60
sabih <siddiquisabih94@gmail.com>
2018-11-05 20:37:36 +0500

    authentication server , login signup using mongoDB

---
+++ authentication server/App.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var app = express()
+var routes = require("./route/routes")
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://localhost/',
+    {
+        // useMongoClient: true
+    })
+
+app.use(bodyParser.json())
+
+routes(app)
+
+app.use((err, req, res, next) => {
+
+    console.log(err.message)
+
+    res.send(err.message)
+    next()
+})
+
+
+module.exports = app
