b54cb5bf8444bdca51be8ced31b0e728afed9809
sabih <siddiquisabih94@gmail.com>
2018-11-09 21:28:32 +0500

    complete apis for upload file and retrive file

--- file upload to mongo db/index.js
+++ file upload to mongo db/index.js
@@ -1,17 +1,15 @@
 var express = require('express');
-var fs = require('fs');
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 var bodyParser = require('body-parser')
-var imgPath = '/path/yourimage.png';
 var app = express()
+
+
 // new
 var path = require('path')
 var crypto = require('crypto')
 var multer = require('multer')
 var GridFsStorage = require('multer-gridfs-storage')
 var Grid = require('gridfs-stream')
-var methodOverride = require('method-override')
 
 
 app.use(bodyParser.json())
@@ -25,10 +23,9 @@ conn.once('open', () => {
 
 // init storage 
 
-
-
-
 const storage = new GridFsStorage({
+
+
     url: 'mongodb://localhost/',
     file: (req, file) => {
         return new Promise((resolve, reject) => {
@@ -55,164 +52,83 @@ const upload = multer({ storage });
 
 
 // upload request 
-
-app.post('/api/uploadFile', upload.single('file'), (req, res) => {
-
+app.post('/api/image/upload', upload.single('file'), (req, res) => {
     res.json({ file: req.file })
 })
 
 
 
-// get file object
-
-app.get('/getFiles', (req, res) => {
+// get all file name and details
+app.get('/api/getAll/images', (req, res) => {
     gfs.files.find().toArray((err, files) => {
         if (!files || files.length === 0) {
             return res.status(404).json({
                 err: 'no file'
             })
         }
-
         return res.json(files)
     })
-
 })
 
 
-// get file
-app.post('/api/images', (req, res) => {
-    console.log(req.body.filename)
-    gfs.files.findOne({ filname: req.body.filename }, (err, file) => {
-
-        console.log(file)
+// get specific file name and other details
+app.get('/api/get/imagesByFilename/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
         if (!file || file.length === 0) {
+            return res.status
             return res.status(404).json({
                 err: 'no file'
             })
         }
+        return res.json(file)
+    })
+})
 
-        if (file.contentType == 'image/jpeg' || file.contentType == 'image/png') {
-            const readStream = gfs.createReadStream(file.filename)
-            readStream.pipe(res)
+
+// read file and get image 
+app.get('/api/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+        if (file) {
+            if (!file || file.length === 0) {
+                return res.status
+                return res.status(404).json({
+                    err: 'no file found'
+                })
+            }
+            if (file.length !== 0 && file) {
+                var readstream = gfs.createReadStream(file.filename);
+                readstream.pipe(res);
+            }
+            else {
+                return res.status(404).json({
+                    err: 'file Error'
+                })
+            }
         }
         else {
             res.status(404).json({
-                err: 'no file found'
+                err: 'no such file'
+            })
+        }
+
+
+
+        if (err) {
+            res.status(404).json({
+                err: 'error while finding file'
             })
         }
     })
 
-})
-
-
-
-
-
-var schema = new Schema({
-    // img: {
-    //     data: {
-    //         type: Buffer
-    //     },
-    //     contentType: {
-    //         type: String
-    //     },
-    // },
-    filePath: {
-        type: String
-    }
-});
-
-var A = mongoose.model('A', schema);
-
-
-
-var nameSave = new Schema({
-    name: {
-        type: String
-    }
-});
-
-var nameSchema = mongoose.model('nameSave', nameSave);
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.post('/api/filePath', (req, res, next) => {
-
-
-
-
-    // A.create({img:{data :req.body.img.data }})
-
-    // res.send(req.body.img.contentType)
-
-
-    var data = fs.readFileSync(A.filePath);
-    // A.img.contentType = 'image/png';
-    // A.save(function (err, a) {
-    //     if (error) {
-    //         res.send(error)
-    //     }
-    //     else {
-    //         res.send(a)
-    //     }
-
-    // })
-
-    if (data) {
-
-        res.send('success')
-    }
-
-
-
-
-
 
 
 
 
 })
 
-app.post('/api/name', (req, respon, next) => {
-
-    respon.send(req.body.name)
-
-
-
-    nameSchema.create({ name: req.body.name })
-        .then((a) => {
-            console.log('saved')
-            respon.send(a)
-        })
-        .catch((error) => {
-            console.log('not saved')
-            respon.send(error)
-        })
-})
-app.get('/api', (req, res, next) => {
-
-    res.send({ send: 'sididqui' })
-
-
-    // A.create({})
-
-})
 
 
 app.use((err, req, res, next) => {
-
-    console.log(err.message)
-
     res.send(err.message)
     next()
 })
@@ -221,50 +137,4 @@ app.use((err, req, res, next) => {
 app.listen(3500, () => {
     console.log('server is running')
 })
-
-
-
-// A.remove(function (err) {
-//     if (err) throw err;
-
-
-
-//     // store an img in binary in mongo
-//     var a = new A;
-//     a.img.data = fs.readFileSync(imgPath);
-//     a.img.contentType = 'image/png';
-//     a.save(function (err, a) {
-//         if (err) throw err;
-
-//         console.error('saved img to mongo');
-
-//         // start a demo server
-//         var server = express.createServer();
-//         server.get('/', function (req, res, next) {
-//             A.findById(a, function (err, doc) {
-//                 if (err) return next(err);
-//                 res.contentType(doc.img.contentType);
-//                 res.send(doc.img.data);
-//             });
-//         });
-
-//         server.on('close', function () {
-//             console.error('dropping db');
-//             mongoose.connection.db.dropDatabase(function () {
-//                 console.error('closing db connection');
-//                 mongoose.connection.close();
-//             });
-//         });
-
-//         server.listen(3333, function (err) {
-//             var address = server.address();
-//             console.error('server listening on http://%s:%d', address.address, address.port);
-//             console.error('press CTRL+C to exit');
-//         });
-
-//         process.on('SIGINT', function () {
-//             server.close();
-//         });
-//     });
-// });
 
